59bf596fc845c720197c5e9ccc1355f5dbf23ef9
Douglas G. Scofield <douglasgscofield@gmail.com>
2015-02-04 14:49:17 +0100

    Update git-getversion.sh for vcflib submodule

--- git-getversion.sh
+++ git-getversion.sh
@@ -3,9 +3,9 @@ revisioncount=`git log --oneline | wc -l | sed -e 's/^\s\+//g' -e 's/\s\+$//g'`
 projectversion=`git describe --tags --long`
 cleanversion=${projectversion%%-*}
 reference=`git config --get remote.origin.url`
-vcfrevisioncount=`(cd ../vcflib && git log --oneline | wc -l | sed -e 's/^\s\+//g' -e 's/\s\+$//g')`
-vcfversion=`(cd ../vcflib && git describe --all --long)`
-vcfreference=`(cd ../vcflib && git config --get remote.origin.url)`
+vcfrevisioncount=`(cd vcflib && git log --oneline | wc -l | sed -e 's/^\s\+//g' -e 's/\s\+$//g')`
+vcfversion=`(cd vcflib && git describe --all --long)`
+vcfreference=`(cd vcflib && git config --get remote.origin.url)`
 
 echo "#define VERSION \"$projectversion-$revisioncount\""
 #echo "#define VERSION \"$cleanversion.$revisioncount\""
